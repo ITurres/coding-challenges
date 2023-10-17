7039f0ef2eb1d0f7cac6ccfa215f5efe50b22c46
iturres <emanueliturres.co@gmail.com>
2023-10-17 00:17:29 -0300

    Refactor: Ruby/ 'diagonal_difference - use '.reduce'

--- Ruby/diagional_difference/diagonal_difference.rb
+++ Ruby/diagional_difference/diagonal_difference.rb
@@ -4,14 +4,27 @@ def diagonal_difference(arr)
   end
 
   matrix_size = arr.length()
-  left_to_right = 0
-  right_to_left = 0
 
-  arr.map do |inner_arr|
-    left_to_right += inner_arr[0 - matrix_size]
-    right_to_left += inner_arr[matrix_size - 1]
+  # ! this code will be left here for reference.
+  #   left_to_right = 0
+  #   right_to_left = 0
+
+  #   arr.map do |inner_arr|
+  #     left_to_right += inner_arr[0 - matrix_size]
+  #     right_to_left += inner_arr[matrix_size - 1]
+  #     matrix_size -= 1
+  #   end
+
+  #   (left_to_right - right_to_left).abs # * handle negatives by returning absolute value of the number.
+  # ! -----------------------------
+
+  diagonals = arr.reduce([0, 0]) do |sums, inner_arr|
+    sums[0] += inner_arr[0 - matrix_size] # ! Left to right.
+    sums[1] += inner_arr[matrix_size - 1] # ! Right to left.
     matrix_size -= 1
+    sums
   end
 
-  (left_to_right - right_to_left).abs # * handle negatives by returning absolute value of the number.
+  # * difference between the two diagonals
+  (diagonals[0] - diagonals[1]).abs # * handle negatives by returning absolute value of the number.
 end
